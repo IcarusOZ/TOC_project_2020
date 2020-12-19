82184b0d9644a4a0ad3202f35d0b33cfa5934555
Eason <supereaonsoccer.tw@gmail.com>
2020-12-19 22:46:07 +0800

    change the host url to correct one

--- app.py
+++ app.py
@@ -152,5 +152,5 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    port = os.environ['PORT']
-    app.run(host="0.0.0.0", port=port, debug=True, reloader=True)
+    PORT = os.environ['PORT']
+    app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
